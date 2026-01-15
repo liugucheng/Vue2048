8f02d4bd6571a0e93a4851ec2007029d6533bc01
liugucheng <171064951@qq.com>
2026-01-15 08:03:35 +0800

    feat: add commit hooks with husky and commitlint

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import Game2048 from './components/Game2048.vue'
+import Game2048 from "./components/Game2048.vue";
 </script>
 
 <template>
   <Game2048 />
+  <!-- <HelloWorld msg="Hello Vue 3!" /> -->
 </template>
